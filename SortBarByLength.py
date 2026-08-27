c542f1f3adb961052ff503d4ad4221dcbbcac5ac
civinko <57294125+civinko@users.noreply.github.com>
2026-08-27 17:22:14 -0400

    Create SortBarByLength.py
    added new question file

---
+++ SortBarByLength.py
@@ -0,0 +1,39 @@
+# Problem: Sort Bars by Length
+#
+# You are given a list of strings where each string contains only
+# the character "I".
+#
+# Sort the strings from shortest to longest based on their length.
+#
+# Example:
+#
+# Input:
+# bars = [
+#     "IIII",
+#     "III",
+#     "IIIIII",
+#     "IIII",
+#     "IIIIIIIII",
+#     "II"
+# ]
+#
+# Output:
+# [
+#     "II",
+#     "III",
+#     "IIII",
+#     "IIII",
+#     "IIIIII",
+#     "IIIIIIIII"
+# ]
+#
+# Write a function:
+#
+# def sortBars(bars):
+#
+# Return the sorted list.
+#
+# Constraints:
+# - The list contains at least one string.
+# - Each string contains only the character "I".
+# - Do not modify the contents of the strings.
